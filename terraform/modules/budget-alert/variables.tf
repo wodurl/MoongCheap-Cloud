8bb3fcc7a7bed2e4cc66c96c7769bc0be9d508ab
Ubuntu <ubuntu@ip-172-31-16-220.ap-northeast-2.compute.internal>
2026-09-09 06:02:20 +0000

    feat(terraform): add AWS Budgets threshold notifications (50/80/100%, $552 limit)

--- terraform/modules/budget-alert/variables.tf
+++ terraform/modules/budget-alert/variables.tf
@@ -9,3 +9,15 @@ variable "discord_webhook_url" {
   type        = string
   sensitive   = true
 }
+
+variable "monthly_budget_limit_usd" {
+  description = "월별 예산 한도 (USD)"
+  type        = string
+  default     = "552"
+}
+
+variable "budget_notification_thresholds" {
+  description = "예산 대비 알림을 보낼 임계값(%) 목록"
+  type        = list(number)
+  default     = [50, 80, 100]
+}
